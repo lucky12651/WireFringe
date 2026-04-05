cd8d24f05d40a0fcefa8d1d31a0f245fa39efc44
Vaibhav Srivastava <vkluckymgs@gmail.com>
2026-04-05 13:26:55 +0530

    adsense added

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/styles.css';
 import '../styles/admin.css';
 
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,6 +10,13 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+      <Script
+        id="google-adsense"
+        async
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9036526646235532"
+        crossOrigin="anonymous"
+      />
       <Component {...pageProps} />
     </>
   );
