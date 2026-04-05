4701dce74e48f36a1b49b8faafc5694ed4a85a15
Vaibhav Srivastava <vkluckymgs@gmail.com>
2026-04-05 13:38:08 +0530

    head tag fix

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -2,21 +2,18 @@ import '../styles/styles.css';
 import '../styles/admin.css';
 
 import Head from 'next/head';
-import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9036526646235532"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
-      <Script
-        id="google-adsense"
-        async
-        strategy="afterInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9036526646235532"
-        crossOrigin="anonymous"
-      />
       <Component {...pageProps} />
     </>
   );
