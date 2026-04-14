e261d0a68b43e49b4fefcfdb08ca4e4592245467
Vaibhav Srivastava <vkluckymgs@gmail.com>
2026-04-14 10:55:39 +0530

    feat: load Google Ads script conditionally in App component and remove from Document

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -5,11 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9036526646235532"
-          crossOrigin="anonymous"
-        ></script>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=DM+Sans:wght@400;500;700&display=swap"
           rel="stylesheet"
